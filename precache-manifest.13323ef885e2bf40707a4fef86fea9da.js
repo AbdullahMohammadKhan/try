7a985c2fa20e8312572c9e644ab67eb58659214a
abdullah <hbkamk@gmail.com>
2020-07-17 12:21:12 +0600

    Updates

---
+++ precache-manifest.13323ef885e2bf40707a4fef86fea9da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5137384d179cbb505802d2f906c3b6f8",
+    "url": "/try/index.html"
+  },
+  {
+    "revision": "f693b6977dd10b273ad9",
+    "url": "/try/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e1edd818ddea32db1cf",
+    "url": "/try/static/js/2.6dd3362f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/try/static/js/2.6dd3362f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f693b6977dd10b273ad9",
+    "url": "/try/static/js/main.bd7fe6bb.chunk.js"
+  },
+  {
+    "revision": "57a7050db913075489a7",
+    "url": "/try/static/js/runtime-main.f9710751.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/try/static/media/logo.5d5d9eef.svg"
+  }
+]);
